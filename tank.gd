2bd6cfbb18706dd1e1b4038d88e467a9f2a64520
MrBocch <jorgealberto1436@gmail.com>
2024-04-09 15:48:00 -0600

    tank movement code done

--- tank.gd
+++ tank.gd
@@ -1,21 +1,27 @@
 extends Node3D
 
-
+@export var SPEED = 5.0
+@export var TURN_RADIUS = deg_to_rad(2.0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("hi")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
-		self.rotation.y += 0.1
+		self.rotation.y += TURN_RADIUS
 	elif Input.is_action_pressed("ui_right"):
-		self.rotation.y -= 0.1
-		
-	# how do i make it so that the tank 
-	# only "moves forward" as in, the 
-	# place its facing in?
-	var forward = global_transform.basis.z.normalized()
-	translate(forward * 0.01)
+		self.rotation.y -= TURN_RADIUS 
+	
+	# trigonometry is my friend
+	if Input.is_action_pressed("ui_up"):
+		self.position.x += sin(self.rotation.y) * SPEED * delta
+		self.position.z += cos(self.rotation.y) * SPEED * delta
+	if Input.is_action_pressed("ui_down"):
+		self.position.x -= sin(self.rotation.y) * SPEED * delta
+		self.position.z -= cos(self.rotation.y) * SPEED * delta
+
+	
+	
+
