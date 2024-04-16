196f771477f4e5a22369709895c1d70aaba0651d
MrBocch <jorgealberto1436@gmail.com>
2024-04-16 11:41:14 -0600

    felt like i got nothing done

--- scenes/tank/tank.gd
+++ scenes/tank/tank.gd
@@ -4,8 +4,10 @@ extends Node3D
 @export var TURN_RADIUS = deg_to_rad(2.0)
 
 @onready var root = get_tree().get_root()
+@onready var barrel_location = $barrel
 
 var bullet
+
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
@@ -30,5 +32,13 @@ func _process(delta):
 	# add count down, not let people shoot tomuch
 	if Input.is_action_just_pressed("fire"):
 		root.add_child(bullet)
-	
+		bullet.position.x = self.position.x
+		bullet.position.z = self.position.z + 1.243
+		bullet.position.y = 1.249 + 0.5
 
+# were is the gun located?, pointing to? 
+func gun_point():
+	# x = 0
+	# y = 1.249 + (radius of bullet)/2?
+	# z = 1.243 
+	pass
